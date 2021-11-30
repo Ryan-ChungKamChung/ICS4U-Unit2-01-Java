8d3b309393c18de636338202de2f31c01fbc0778
Ubuntu <ubuntu@ip-172-31-92-172.ec2.internal>
2021-11-30 17:40:36 +0000

    added .java files

---
+++ ReverseString.java
@@ -0,0 +1,79 @@
+/*
+* This is a program that calculates mean, median and mode
+* after reading in a text file into an array.
+*
+* @author  Ryan Chung
+* @version 1.0
+* @since   2021-10-26
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+* This is the statistics program.
+*/
+final class ReverseString {
+
+    /**
+    * Prevent instantiation
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private ReverseString() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The mean() function.
+    *
+    * @param inputString is the user-inputted string.
+    * @return the substring of the inputted string with the first character
+    *       placed at the back of the string
+    */
+    public static String reverse(final String inputString) {
+
+        final String returnValue;
+
+        if (inputString.length() == 0) {
+            returnValue = inputString;
+        } else {
+            returnValue = reverse(inputString.substring(1))
+                + inputString.charAt(0);
+        }
+
+        return returnValue;
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+
+        final String inputString;
+        final String reversedString;
+
+        try {
+
+            // User prompt
+            System.out.print("Enter a string: ");
+
+            // Gathers and parses input to a String
+            inputString = new BufferedReader(
+                    new InputStreamReader(System.in)
+            ).readLine();
+
+            reversedString = reverse(inputString);
+            System.out.println(reversedString);
+        } catch (IOException exception) {
+            System.out.println("Please enter a string.");
+        }
+    }
+}
+
